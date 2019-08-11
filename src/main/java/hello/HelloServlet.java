254b5a8ff3c0206c8e0c18a3d6b77bd926e113bf
JustynaG88 <wardaszka.justyna@gmail.com>
2019-08-11 14:44:48 +0200

    all

--- src/main/java/hello/HelloServlet.java
+++ src/main/java/hello/HelloServlet.java
@@ -1,6 +1,7 @@
 package hello;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -8,12 +9,19 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "HelloServlet", value = "/hello")
+@WebServlet(name = "HelloServlet",
+        urlPatterns = {"/hello", "/czesc", "/ola!"},
+        initParams = {
+                @WebInitParam(name = "who", value = "world"),
+                @WebInitParam(name = "times", value = "5")
+        }
+)
 public class HelloServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         PrintWriter out = response.getWriter();
-        out.println("Hello World!");
+        out.println("who ?" + getInitParameter("who"));
+        out.println("how many times will u disturb me ?" + getInitParameter("times"));
     }
 }
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: KOMP
+  Date: 2019-08-11
+  Time: 13:46
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<p>Adrian to jest straszny cygan</p>
+</body>
+</html>
